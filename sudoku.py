0e9c47c36876aa53579d790685ad5bbc10e46fce
Robin Schmid <schmidrobin@outlook.com>
2024-07-13 01:08:07 +0200

    Add global params

--- sudoku.py
+++ sudoku.py
@@ -1,27 +1,45 @@
 """
-Sudoko Solver
+Sudoko Solver with zero imports
 Robin Schmid
 """
 
+MAX_NUM_IT = 200 # Max number of iterations before it should stop
+INPUT_GAME = False  # True if enter initial game state via terminal input, False to load hardcoded
+
+# Initial game state, gets loaded when INPUT_GAME is set to False
+INIT_GAME_STATE = [[0, 6, 2, 3, 4, 0, 7, 5, 0],
+             [1, 0, 0, 0, 0, 5, 6, 0, 0],
+             [5, 7, 0, 0, 0, 0, 0, 4, 0],
+             [0, 0, 0, 0, 9, 4, 8, 0, 0],
+             [4, 0, 0, 0, 0, 0, 0, 0, 6],
+             [0, 0, 5, 8, 3, 0, 0, 0, 0],
+             [0, 3, 0, 0, 0, 0, 0, 9, 1],
+             [0, 0, 6, 4, 0, 0, 0, 0, 7],
+             [0, 5, 9, 0, 8, 3, 2, 6, 0]]
+
+
 class Sudoku():
     def __init__(self):
+        """Initializes the game with all 0, unkowns"""
         self.board = []
         row = []
-        for i in range(0, 9):
+        for _ in range(0, 9):
             row.append(0)
-        for j in range(0, 9):
+        for _ in range(0, 9):
             self.board.append(row)
 
     def check_finished(self):
-        count = 0
+        """Checks if the game is solved"""
+        cnt = 0
         for i in range(0,9):
             for j in range(0,9):
                 if self.board[i][j] == 0:
-                    count += 1
-        if count == 0:
+                    cnt += 1
+        if cnt == 0:
             return 1
 
     def enter_game(self):
+        """Enters the initial game state, 0 is unknown"""
         for i in range(0, 9):
             print(f"Enter the row {i+1}/9 and press enter after each number.")
             j = 0
@@ -35,6 +53,7 @@ class Sudoku():
                     j += 1
 
     def print_board(self):
+        """Prints the board"""
         if self.board == None:
             print("No board loaded")
         else:
@@ -52,6 +71,8 @@ class Sudoku():
             print("\n")
 
     def logic(self, i_, j_):
+        """Simple logic to iterate through rows, columns 
+        and blocks and adds the only possible number"""
         pos_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         i_block = (i_ // 3) * 3
         j_block = (j_ // 3) * 3
@@ -81,45 +102,18 @@ class Sudoku():
             self.board[i_][j_] = pos_num[0]
             self.print_board()
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     sud = Sudoku()
 
-    max_num_games = 200
+    if INPUT_GAME:
+        sud.enter_game()
+        sud.print_board()
+    else:
+        sud.board = INIT_GAME_STATE
 
-    # sud.start = [[0, 0, 0, 6, 0, 2, 0, 0, 0],
-    #          [4, 0, 0, 0, 5, 0, 0, 0, 1],
-    #          [0, 8, 5, 0, 1, 0, 6, 2, 0],
-    #          [0, 3, 8, 2, 0, 6, 7, 1, 0],
-    #          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #          [0, 1, 9, 4, 0, 7, 3, 5, 0],
-    #          [0, 2, 6, 0, 4, 0, 5, 3, 0],
-    #          [9, 0, 0, 0, 2, 0, 0, 0, 7],
-    #          [0, 0, 0, 8, 0, 9, 0, 0, 0]]
-
-    # sud.end =   [[1, 9, 3, 6, 7, 2, 4, 8, 5],
-    #          [4, 6, 2, 3, 5, 8, 9, 7, 1],
-    #          [7, 8, 5, 9, 1, 4, 6, 2, 3],
-    #          [5, 3, 8, 2, 9, 6, 7, 1, 4],
-    #          [6, 7, 4, 1, 3, 5, 2, 9, 6],
-    #          [2, 1, 9, 4, 8, 7, 3, 5, 8],
-    #          [8, 2, 6, 7, 4, 1, 5, 3, 9],
-    #          [9, 4, 1, 5, 2, 3, 8, 6, 7],
-    #          [3, 5, 7, 8, 6, 9, 1, 4, 2]]
-
-    sud.start = [[0, 6, 2, 3, 4, 0, 7, 5, 0],
-             [1, 0, 0, 0, 0, 5, 6, 0, 0],
-             [5, 7, 0, 0, 0, 0, 0, 4, 0],
-             [0, 0, 0, 0, 9, 4, 8, 0, 0],
-             [4, 0, 0, 0, 0, 0, 0, 0, 6],
-             [0, 0, 5, 8, 3, 0, 0, 0, 0],
-             [0, 3, 0, 0, 0, 0, 0, 9, 1],
-             [0, 0, 6, 4, 0, 0, 0, 0, 7],
-             [0, 5, 9, 0, 8, 3, 2, 6, 0]]
-
-    start = sud.enter_game()
-
-    for n in range(0, max_num_games):
+    # Iterate through all options
+    for n in range(0, MAX_NUM_IT):
         for i in range(0,9):
             for j in range(0,9):
                 if sud.board[i][j] == 0:
@@ -127,5 +121,5 @@ if __name__ == '__main__':
 
         if sud.check_finished():
             print("Game finished!")
-            print("Number of games: {}".format(n))
+            print(f"Total number of iterations through whole board: {n}")
             exit()
